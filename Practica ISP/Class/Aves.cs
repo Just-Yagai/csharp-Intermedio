78e85719b2629b0ba0a1e69ad7896baf68efa0c7
Sirius <romerperez26@gmail.com>
2024-08-05 22:07:55 -0400

    Practica ISP

---
+++ Practica ISP/Class/Aves.cs
@@ -0,0 +1,38 @@
+﻿using Practica_ISP.Interface;
+
+namespace Practica_ISP.Class
+{
+    public class Aves
+    {
+        public class Pato : IAve, INadar
+        {
+            public void Nadar()
+            {
+                throw new NotImplementedException();
+            }
+
+            public void PonerHuevos()
+            {
+                throw new NotImplementedException();
+            }
+
+            public void Volar()
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public class Pinguino : IAve, INadar
+        {
+            public void Nadar()
+            {
+                throw new NotImplementedException();
+            }
+
+            public void PonerHuevos()
+            {
+                throw new NotImplementedException();
+            }
+        }
+    }
+}
---
+++ Practica ISP/Interface/IAve.cs
@@ -0,0 +1,22 @@
+﻿namespace Practica_ISP.Interface
+{
+    public interface IAve
+    {
+        void PonerHuevos();
+    }
+
+    public interface INadar
+    {
+        void Nadar();
+    }
+
+    public interface ICazar
+    {
+        void Cazar();
+    }
+
+    public interface IVolar
+    {
+        void Volar();
+    }
+}
